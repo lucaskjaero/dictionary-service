a079456dc9d7c813ae2880639eb77d8a0b1cb5e1
Lucas Kjaero <lucas@lucaskjaero.com>
2018-05-15 07:41:04 -0700

    Response code

--- NaiveDictionaryService.py
+++ NaiveDictionaryService.py
@@ -18,23 +18,30 @@ class WordHandler(Resource):
         entries = []
         message = ""
         status = "OK"
+        response_code = 200
 
         try:
             if language == "chinese":
                 entries, message = get_definition(word)
                 LOGGER.info("Getting definition for %s"  % word)
+
+                if len(entries) == 0:
+                    response_code = 404
+
             else:
                 status = "ERROR"
                 message = "Language %s has not been implemented yet." % language
                 LOGGER.error("Customer tried to get definitions in %s" % language)
+                response_code = 400
 
         except exception as e:
             status = "ERROR"
             message = "An error has occurred"
 
             LOGGER.error("Error getting word %s: %s" % (word, e))
+            response_code = 500
 
-        return {"status": status, "message": message, "entries": entries}
+        return {"status": status, "message": message, "entries": entries}, response_code
 
 api.add_resource(WordHandler, '/v1/<string:language>/definition/<string:word>')
 api.add_resource(HSKHandler, '/v1/chinese/hsk/<int:level>')
