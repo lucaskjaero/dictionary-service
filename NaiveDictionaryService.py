eda16a65cc31f34c08f8ccbcf26f45d1c45f7dd2
Lucas Kjaero <lucas@lucaskjaero.com>
2018-12-07 23:19:35 -0800

    Allow custom rewriting of the url path base

--- NaiveDictionaryService.py
+++ NaiveDictionaryService.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_restful import Resource, Api
 
@@ -5,6 +7,8 @@ from chinese.Chinese import get_definition, HSKHandler
 
 app = Flask(__name__)
 api = Api(app)
+
+API_BASE = os.getenv('APPLICATION_ROOT', "/")
 
 
 class WordHandler(Resource):
@@ -20,21 +24,21 @@ class WordHandler(Resource):
                 entries, message = get_definition(word)
                 print("Getting definition for %s" % word)
 
-                if len(entries) == 0:
-                    response_code = 404
-
             else:
                 status = "ERROR"
                 message = "Language %s has not been implemented yet." % language
                 print("Customer tried to get definitions in %s" % language)
                 response_code = 400
 
-        except exception as e:
+        except Exception as e:
             status = "ERROR"
             message = "An error has occurred"
 
             print("Error getting word %s: %s" % (word, e))
             response_code = 500
+
+        if len(entries) == 0:
+            response_code = 404
 
         return {"status": status, "message": message, "entries": entries}, response_code
 
@@ -44,8 +48,8 @@ class HealthHandler(Resource):
         return "Dictionary service is up", 200
 
 
-api.add_resource(WordHandler, '/v1/<string:language>/definition/<string:word>')
-api.add_resource(HSKHandler, '/v1/chinese/hsk/<int:level>')
+api.add_resource(WordHandler, API_BASE + '/v1/<string:language>/definition/<string:word>')
+api.add_resource(HSKHandler, API_BASE + '/v1/chinese/hsk/<int:level>')
 api.add_resource(HealthHandler, '/')
 
 if __name__ == '__main__':
