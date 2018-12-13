73373a0da05b7ff5e2d5a27882afa4f75bcdb4f5
Lucas Kjaero <lucas@lucaskjaero.com>
2018-12-12 21:39:39 -0800

    Correct route

--- routes.ex
+++ routes.ex
@@ -1,9 +1,9 @@
-scope "/api/v1/", DictionaryServiceWeb do
-    pipe_through(:api)
+scope "/v1/", DictionaryServiceWeb do
+  pipe_through(:api)
 
-    # Vocab lookup
-    get("/:language/definition/:word", VocabularyController, :request)
+  # Vocab lookup
+  get("/:language/definition/:word", VocabularyController, :request)
 
-    # Level lists by language
-    get("/chinese/hsk/:level", HSKController, :request)
-  end
+  # Level lists by language
+  get("/chinese/hsk/:level", HSKController, :request)
+end
